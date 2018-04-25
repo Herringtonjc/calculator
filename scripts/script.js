2ca6e95ea6f6e9fe0aef83db53c3dd8220621102
Jarred Herrington <herringtonjc@yahoo.com>
2018-04-25 02:21:09 -0500

    Change styles

--- scripts/script.js
+++ scripts/script.js
@@ -6,99 +6,115 @@ const numberNodes = document.querySelectorAll(".number-button");
 let displayValue = "";
 let smallDisplayValue = "";
 let bigDisplayValue = "";
-let operatorValue = "";
+let doneOperating = false;
 
 numberNodes.forEach(function(button) {
-  button.addEventListener("click", buttonListener);
+    button.addEventListener("click", buttonListener);
 })
 
 function buttonListener(event) {
-  switch (event.target.getAttribute("id")) {
-    case "button-clear":
-      clearDisplay();
-      break;
-    case "button-backspace":
-      eraseDisplay();
-      break;
-    case "button-divide":
-        displayValue += "/";
-        bigDisplayValue += event.target.textContent;
-        smallDisplayValue += event.target.textContent;
-        updateDisplay();
-        break;
-    case "button-multiply":
-        displayValue += "*";
-        bigDisplayValue += event.target.textContent;
-        smallDisplayValue += event.target.textContent;
-        updateDisplay();
-        break;
-    case "button-subtract":
-        displayValue += "-";
-        bigDisplayValue += event.target.textContent;
-        smallDisplayValue += event.target.textContent;
-        updateDisplay();
-        break;
-    case "button-add":
-        displayValue += "+";
-        bigDisplayValue += event.target.textContent;
-        smallDisplayValue += event.target.textContent;
-        updateDisplay();
-        break;
-    case "button-0":
-    case "button-1":
-    case "button-2":
-    case "button-3":
-    case "button-4":
-    case "button-5":
-    case "button-6":
-    case "button-7":
-    case "button-8":
-    case "button-9":
-      bigDisplayValue += event.target.textContent;
-      smallDisplayValue += event.target.textContent;
-      displayValue += event.target.textContent;
-      updateDisplay();
-      break;
-    case "button-equals":
-      operate();
-      break;
-    case "button-period":
-      isDecimal();
-      break;
-  }
+    switch (event.target.getAttribute("id")) {
+        case "button-clear":
+            clearDisplay();
+            break;
+        case "button-backspace":
+            eraseDisplay();
+            break;
+        case "button-divide":
+            updateOperator("/");
+            break;
+        case "button-multiply":
+            updateOperator("*");
+            break;
+        case "button-subtract":
+            updateOperator("-");
+            break;
+        case "button-add":
+            updateOperator("+");
+            break;
+        case "button-0":
+        case "button-1":
+        case "button-2":
+        case "button-3":
+        case "button-4":
+        case "button-5":
+        case "button-6":
+        case "button-7":
+        case "button-8":
+        case "button-9":
+            if (doneOperating) {
+                clearDisplay();
+                doneOperating = false;
+                bigDisplayValue += event.target.textContent;
+                smallDisplayValue += event.target.textContent;
+                displayValue += event.target.textContent;
+                updateDisplay();
+            } else {
+                doneOperating = false;
+                bigDisplayValue += event.target.textContent;
+                smallDisplayValue += event.target.textContent;
+                displayValue += event.target.textContent;
+                updateDisplay();
+            }
+            break;
+        case "button-equals":
+            operate();
+            break;
+        case "button-period":
+            isDecimal();
+            break;
+    }
 }
 
 function updateDisplay() {
-  bigDisplay.textContent = bigDisplayValue;
-  smallDisplay.textContent = smallDisplayValue;
+    bigDisplay.textContent = bigDisplayValue;
+    smallDisplay.textContent = smallDisplayValue;
 }
 
 function clearDisplay() {
-  displayValue = "";
-  bigDisplayValue = "";
-  smallDisplayValue = "";
-  operator = "";
-  updateDisplay();
+    displayValue = "";
+    bigDisplayValue = "";
+    smallDisplayValue = "";
+    operator = "";
+    updateDisplay();
 }
 
-function isDecimal() {
-  if (!bigDisplayValue.match(/[.]/g)) {
+function updateOperator(operator) {
+    doneOperating = false;
+    displayValue += operator;
     bigDisplayValue += event.target.textContent;
     smallDisplayValue += event.target.textContent;
     updateDisplay();
-  }
+}
+
+function isDecimal() {
+    if (!bigDisplayValue.match(/[.]/g)) {
+        bigDisplayValue += event.target.textContent;
+        smallDisplayValue += event.target.textContent;
+        updateDisplay();
+    }
 }
 
 function eraseDisplay() {
-  if (bigDisplayValue != "") {
-    bigDisplayValue = bigDisplayValue.slice(0, -1);
-    smallDisplayValue = smallDisplayValue.slice(0, -1);
-    updateDisplay();
-  }
+    if (bigDisplayValue != "") {
+        bigDisplayValue = bigDisplayValue.slice(0, -1);
+        smallDisplayValue = smallDisplayValue.slice(0, -1);
+        displayValue = displayValue.slice(0, -1);
+        updateDisplay();
+    }
 }
 
 function operate(params) {
-    smallDisplayValue += "=";
+    smallDisplayValue += "=" + eval(displayValue);
     bigDisplayValue = eval(displayValue);
-    updateDisplay();
+    if (!isFinite(bigDisplayValue)) {
+        bigDisplayValue = "Black Hole Created";
+        bigDisplay.style.fontSize = "35px";
+        updateDisplay();
+        doneOperating = true;
+    } else {
+        bigDisplayValue = +bigDisplayValue.toFixed(6);
+        updateDisplay();
+        doneOperating = true;
+    }
 }
