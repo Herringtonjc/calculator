9016f96835cadef9b04883852a14ca3729e57bf8
Jarred Herrington <herringtonjc@yahoo.com>
2018-04-26 23:03:26 -0500

    Add keyboard support, and fix inability to do multiple decimal values

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,3 @@
-/*TODO:
- Add keyboard support
-*/
-
 /*////////////////////////////////Selectors///////////////////////////////////*/
 const bigDisplay = document.querySelector("#big-display");
 const smallDisplay = document.querySelector("#small-display");
@@ -16,62 +12,130 @@ let doneOperating = false;
 
 /*////////////////////////////////Event Listeners/////////////////////////////*/
 numberNodes.forEach(function(button) {
-    button.addEventListener("click", buttonListener);
+  button.addEventListener("click", buttonListener);
 })
 
+document.addEventListener("keydown", keyListener);
+
 /*////////////////////////////////Functions///////////////////////////////////*/
+function keyListener(event) {
+  console.log(event.which);
+  switch (event.which) {
+    case 67:
+      clearDisplay();
+      break;
+    case 8:
+      eraseDisplay();
+      break;
+    case 111:
+      updateOperator("/");
+      break;
+    case 106:
+      updateOperator("*");
+      break;
+    case 109:
+      updateOperator("-");
+      break;
+    case 107:
+      updateOperator("+");
+      break;
+    case 96:
+    case 48:
+    case 97:
+    case 49:
+    case 98:
+    case 50:
+    case 99:
+    case 51:
+    case 100:
+    case 52:
+    case 101:
+    case 53:
+    case 102:
+    case 54:
+    case 103:
+    case 55:
+    case 104:
+    case 56:
+    case 105:
+    case 57:
+      if (doneOperating) {
+          clearDisplay();
+          doneOperating = false;
+          bigDisplayValue += event.key;
+          smallDisplayValue += event.key;
+          displayValue += event.key;
+          updateDisplay();
+      } else {
+          doneOperating = false;
+          bigDisplayValue += event.key;
+          smallDisplayValue += event.key;
+          displayValue += event.key;
+          updateDisplay();
+      }
+      break;
+    case 13:
+      operate();
+      break;
+    case 110:
+    case 190:
+      isDecimal();
+      break;
+  }
+}
+
 function buttonListener(event) {
-    switch (event.target.getAttribute("id")) {
-        case "button-clear":
-            clearDisplay();
-            break;
-        case "button-backspace":
-            eraseDisplay();
-            break;
-        case "button-divide":
-            updateOperator("/");
-            break;
-        case "button-multiply":
-            updateOperator("*");
-            break;
-        case "button-subtract":
-            updateOperator("-");
-            break;
-        case "button-add":
-            updateOperator("+");
-            break;
-        case "button-0":
-        case "button-1":
-        case "button-2":
-        case "button-3":
-        case "button-4":
-        case "button-5":
-        case "button-6":
-        case "button-7":
-        case "button-8":
-        case "button-9":
-            if (doneOperating) {
-                clearDisplay();
-                doneOperating = false;
-                bigDisplayValue += event.target.textContent;
-                smallDisplayValue += event.target.textContent;
-                displayValue += event.target.textContent;
-                updateDisplay();
-            } else {
-                doneOperating = false;
-                bigDisplayValue += event.target.textContent;
-                smallDisplayValue += event.target.textContent;
-                displayValue += event.target.textContent;
-                updateDisplay();
-            }
-            break;
-        case "button-equals":
-            operate();
-            break;
-        case "button-period":
-            isDecimal();
-            break;
-    }
+  switch (event.target.getAttribute("id")) {
+    case "button-clear":
+        clearDisplay();
+        break;
+    case "button-backspace":
+        eraseDisplay();
+        break;
+    case "button-divide":
+        updateOperator("/");
+        break;
+    case "button-multiply":
+        updateOperator("*");
+        break;
+    case "button-subtract":
+        updateOperator("-");
+        break;
+    case "button-add":
+        updateOperator("+");
+        break;
+    case "button-0":
+    case "button-1":
+    case "button-2":
+    case "button-3":
+    case "button-4":
+    case "button-5":
+    case "button-6":
+    case "button-7":
+    case "button-8":
+    case "button-9":
+      if (doneOperating) {
+        clearDisplay();
+        doneOperating = false;
+        bigDisplayValue += event.target.textContent;
+        smallDisplayValue += event.target.textContent;
+        displayValue += event.target.textContent;
+        updateDisplay();
+    } else {
+        doneOperating = false;
+        bigDisplayValue += event.target.textContent;
+        smallDisplayValue += event.target.textContent;
+        displayValue += event.target.textContent;
+        updateDisplay();
+      }
+      break;
+    case "button-equals":
+        operate();
+        break;
+    case "button-period":
+        isDecimal();
+        break;
+  }
 }
 
 function updateDisplay() {
@@ -91,7 +155,11 @@ function updateOperator(operator) {
     doneOperating = false;
     displayValue += operator;
     bigDisplayValue = "";
-    smallDisplayValue += event.target.textContent;
+    if (event.type == "click") {
+      smallDisplayValue += event.target.textContent;
+    } else {
+      smallDisplayValue += event.key;
+    }
     updateDisplay();
 }
 
@@ -105,7 +173,7 @@ function isDecimal() {
 }
 
 function eraseDisplay() {
-    if (bigDisplayValue != "") {
+    if (displayValue != "") {
         bigDisplayValue = bigDisplayValue.slice(0, -1);
         smallDisplayValue = smallDisplayValue.slice(0, -1);
         displayValue = displayValue.slice(0, -1);
@@ -113,7 +181,7 @@ function eraseDisplay() {
     }
 }
 
-function operate(params) {
+function operate() {
     smallDisplayValue += "=" + eval(displayValue);
     bigDisplayValue = eval(displayValue);
     if (!isFinite(bigDisplayValue)) {
